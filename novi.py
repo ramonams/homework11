a275b7f5269e86bddfb7f39feab69d67224f6100
ramonams <101112744+ramonams@users.noreply.github.com>
2022-04-27 11:03:00 +0200

    Add files via upload

---
+++ novi.py
@@ -0,0 +1,43 @@
+import datetime
+import json
+import random
+
+secret = random.randint(1, 30)
+attempts = 0
+
+
+with open("score_list.json", "r") as score_file:
+    score_list = json.loads(score_file.read())
+    print("top scores:" + str(score_list))
+
+name = input("Insert your name:")
+"""
+for score_dict in score_list:
+    print(str(score_dict["attempts"]) + "attempts,date:" + score_dict["date"])
+
+"""
+
+while True:
+    guess = int(input("guess the secret number (1-30)"))
+    attempts = attempts + 1
+    wrong_guesses = attempts - 1
+
+    if guess == secret:
+        score_list.append({"name": name,
+                           "attempts ": attempts,
+                           "date ": str(datetime.datetime.now()),
+                           "wrong guesses": str(wrong_guesses)
+                           })
+
+        with open("score_list.json", "w") as score_file:
+            score_file.write(json.dumps(score_list))
+
+        print("you have guessed. it is number: " + str(secret))
+        print("attempts needed:" + str(attempts))
+        print("wrong guesses:" + str(wrong_guesses))
+        break
+
+    elif guess > secret:
+        print("Your guess is not correct. Try smaller.")
+    elif guess < secret:
+        print("Your guess is not correct. Try bigger.")
